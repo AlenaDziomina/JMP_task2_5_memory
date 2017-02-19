d2800b1b235f1cbb80c6029946fdbc3a4ad985e0
Alena Grouk <groukhelena@gmail.com>
2017-02-19 17:19:34 +0300

    memory leak

---
+++ src/com/grouk/Main.java
@@ -0,0 +1,31 @@
+package com.grouk;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    private static ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String path = ClassLoader.getSystemResource("data.txt").getPath();
+        readFile(path);
+    }
+
+    private static void readFile(String name) {
+        try(BufferedReader br = new BufferedReader(new FileReader(new File(name)))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                processLine(line);
+            }
+            Thread.sleep(1000000);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    private static void processLine(String line) {
+        for (int i = 0; i < line.length(); i = i + 4) {
+            String subString = line.substring(i, i + 3);
+            list.add(subString);
+        }
+    }
+}
